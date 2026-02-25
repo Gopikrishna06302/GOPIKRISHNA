40f98849c34db35020f6c355380fbab45d47051e
Gopikrishna06302 <govadagopikrishna958@gmail.com>
2026-02-25 12:52:18 +0530

    Create uart-interrupt.c

---
+++ uart-interrupt.c
@@ -0,0 +1,5 @@
+.\uart_interrupts.o: uart_interrupts.c
+.\uart_interrupts.o: C:\keil_ARM\ARM\Inc\Philips\lpc21xx.h
+.\uart_interrupts.o: defines.h
+.\uart_interrupts.o: delay1.h
+.\uart_interrupts.o: types.h
